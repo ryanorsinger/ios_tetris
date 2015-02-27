8f3ee78d11af7df4c9966ff51e91e066b95431e9
ryanorsinger <ryan.orsinger@gmail.com>
2015-02-27 15:59:53 -0600

    Add scene, configure view, create scene, present scene to GameViewController.swift

--- tetris/GameViewController.swift
+++ tetris/GameViewController.swift
@@ -11,10 +11,23 @@ import SpriteKit
 
 class GameViewController: UIViewController {
 
+    var scene: GameScene!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
 
+    // Configure the view
+    let skView = view as SKView
+    skView.multipleTouchEnabled = false
+
+    // Create and configure the scene
+    scene = GameScene(size: skView.bounds.size)
+    scene.scaleMode = .AspectFill
+    
+    // Present the scene
+        skView.presentScene(scene)
+    }
+    
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
